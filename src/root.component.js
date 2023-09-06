3c0ed8797d82e767a7bb85cb3c7b4a9e721ea75c
mune-kyun <rzknurfaizi@gmail.com>
2023-09-06 22:25:04 +0700

    add react router

--- src/root.component.js
+++ src/root.component.js
@@ -1,3 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <Router>
+      <div>
+        <h1>{props.name}</h1>
+
+        <Routes>
+          <Route path="/contact" element={<div>contact</div>} />
+          <Route path="/" element={<div>home</div>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
